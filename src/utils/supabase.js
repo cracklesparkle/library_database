c4b9a0b6c4c1aca530c239df129b664c07113d3d
cracklesparkle <kitkatt2014@gmail.com>
2024-04-11 10:40:37 +0900

    Key

--- src/utils/supabase.js
+++ src/utils/supabase.js
@@ -1,6 +1,7 @@
 import { createClient } from '@supabase/supabase-js';
-
-const supabase = createClient(process.env.NEXT_PUBLIC_SUPABASE_URL, process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY);
+const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL
+const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY
+const supabase = createClient(supabaseUrl, supabaseKey);
 
 // Function to insert a new book
 export const insertBook = async (bookData) => {
